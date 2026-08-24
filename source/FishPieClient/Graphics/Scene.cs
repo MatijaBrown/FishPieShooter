6b2aa3316a1968df326e9cbb15a170d6f6128d1a
MatijaBrown <matija.brown@outlook.de>
2026-08-24 22:49:14 +0200

    Added Scene class

--- source/FishPieClient/Graphics/Scene.cs
+++ source/FishPieClient/Graphics/Scene.cs
@@ -1,6 +1,22 @@
+using FishPieClient.Graphics.Mesh;
+using Silk.NET.OpenGL;
+
 namespace FishPieClient.Graphics;
 
-public class Scene
+public class Scene : IDisposable
 {
+
+    public List<Entity> Entities { get; } = [];
     
+    public MeshManager MeshManager { get; }
+
+    public Scene(GL gl)
+    {
+        MeshManager = new MeshManager(gl);
+    }
+
+    public void Dispose()
+    {
+        MeshManager.Dispose();
+    }
 }
