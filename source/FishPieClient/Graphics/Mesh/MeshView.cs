a39bc4dd2a71d5066fc9b903dd6f44069876da92
MatijaBrown <matija.brown@outlook.de>
2026-09-15 13:30:08 +0200

    Store vertex and index data separately

--- source/FishPieClient/Graphics/Mesh/MeshView.cs
+++ source/FishPieClient/Graphics/Mesh/MeshView.cs
@@ -3,4 +3,9 @@ using System.Runtime.InteropServices;
 namespace FishPieClient.Graphics.Mesh;
 
 [StructLayout(LayoutKind.Sequential)]
-public readonly record struct MeshView(uint Offset, uint Count);
+public readonly record struct MeshView(
+    uint IndexOffset,
+    uint IndexCount,
+    uint VertexOffset,
+    uint VertexCount
+);
